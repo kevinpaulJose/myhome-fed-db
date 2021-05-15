cf19c277671e10558537cdd42512ff1d10511bf6
kevinjosephpaultwa <kevinjosephpaul.twa@gmail.com>
2021-05-15 20:17:14 +0530

    UPLOAD ARCH

--- myhome/src/Components/Dashboard/DashboardComponent.js
+++ myhome/src/Components/Dashboard/DashboardComponent.js
@@ -1,18 +1,57 @@
 import React from "react";
+import FormData from "form-data";
 
 class DashboardComponent extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      username: "kevin",
+      userFile: "",
+      sample: "Sample",
     };
+    this.uploadForm = this.uploadForm.bind(this);
+  }
+  uploadForm(e) {
+    e.preventDefault();
+    var formData = new FormData();
+    console.log(this.state.userFile);
+    for (let i = 0; i < this.state.userFile.length; i++) {
+      // return;
+      console.log(this.state.userFile[i]);
+      formData.append(
+        "userFile",
+        this.state.userFile[i],
+        this.state.userFile[i].name
+      );
+    }
+    fetch("http://localhost:3001/api/v1/upload", {
+      body: formData,
+      method: "post",
+      headers: {
+        primarypath: "../public",
+        filepath: "/Kevin/Series/Timon",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data.response));
   }
   render() {
-    console.log(this.props);
+    // console.log(this.props);
     return (
       <div>
-        <h1>{this.state.username}</h1>
+        <form onSubmit={this.uploadForm}>
+          <input
+            type="file"
+            name="userFile"
+            multiple
+            id="userFile"
+            onChange={(e) => {
+              this.setState({ userFile: e.target.files });
+              // console.log(this.state);
+            }}
+          />
+          <button type="submit">Submit </button>
+        </form>
       </div>
     );
   }
