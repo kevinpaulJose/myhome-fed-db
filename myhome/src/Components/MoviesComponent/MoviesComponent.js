87d176d472cb929f8eecc670ff53de7754bdb668
kevinjosephpaultwa <kevinjosephpaul.twa@gmail.com>
2021-05-21 21:39:05 +0530

    TEST

--- myhome/src/Components/MoviesComponent/MoviesComponent.js
+++ myhome/src/Components/MoviesComponent/MoviesComponent.js
@@ -5,27 +5,42 @@ class MoviesComponent extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      movies: [],
+    };
   }
 
   getMovies = () => {
     fetch("http://myhome.com:3000/api/v1/media/fetchByType", {
-      body: {
+      body: JSON.stringify({
         user: "kevin",
         password: "kevin",
         server: "myhome.com",
         username: "kevin",
         showtype: "Movies",
-      },
+      }),
       method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
     })
       .then((response) => response.json())
-      .then((data) => console.log(data.message))
+      .then((data) => {
+        // console.log(data);
+        if (data.response) {
+          this.setState({ movies: data.rows });
+          console.log(this.state.movies);
+        }
+      })
       .catch((err) => console.log(err.message));
   };
 
-  render() {
+  componentDidMount() {
     this.getMovies();
+  }
+
+  render() {
     return (
       <div>
         <Banner URL="http://uat.myhome.com:3002/Kevin/Movies/Marvel_Chronology/Cancelling_CC_profile_showing_Admin_page.mp4" />
